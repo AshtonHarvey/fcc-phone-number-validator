0c428f0428d354b2f530e5d36dfca72cb19040b7
Ashton Harvey <Ashtonh36@gmail.com>
2022-11-02 19:18:52 -0400

    Project fails tests and needs rewriting. Possible to validate much more cleanly with only regex

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,8 +1,36 @@
 let input = document.getElementById("phone");
-let output = document.getElementById("output");
+let output = document.getElementById("answer");
 
 function handleSubmit() {
-  validateNumber();
+  //writing it this way so that i can use this code in freecodecamp project page
+  output.innerHTML = validateNumber(input.value.trim());
 }
 
-function validateNumber() {}
+function validateNumber(number) {
+  let regex = /[!-'*-,./:-~]/gi;
+  let regexNumbers = /[0-9]/gi;
+  let symbols = number.match(regex);
+  let extractedNumber = number.match(regexNumbers);
+  let numbers = number.split("");
+
+  console.log(symbols);
+  console.log(extractedNumber);
+  console.log(numbers);
+
+  if (
+    symbols === null &&
+    (extractedNumber.length === 10 ||
+      (extractedNumber.length === 11 && numbers[0] == 1))
+  ) {
+    //validate as true
+    return true;
+  } else {
+    // validate as false
+    return false;
+  }
+}
+
+// PAIN
+//rewrite using regex to validate opening and closing () as well as number of numbers before or after - ?
+// use original input "number"
+// write function to whole everything in the if
